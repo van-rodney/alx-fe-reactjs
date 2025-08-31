c24257d0a4e26bce33e0559cde50fb65e0dd3e57
verb <vanbrght@gmail.com>
2025-08-31 23:18:51 +0000

    Fix: Move App.jsx to src folder so autograder can find it

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 // Imports
-import WelcomeMessage from './WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,8 +1,8 @@
 function WelcomeMessage() {
   return (
     <div>
-      <h1>Hello everyone, I am learning React at ALX!</h1>
-      <p>I am learning about JSX!</p>
+      <h1>"Hello everyone, I am learning React at ALX!"</h1>
+      <p>"I am learning about JSX!"</p>
     </div>
   );
 }

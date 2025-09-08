b47d57c5181a9ddaffd270908a6f5618817a8452
verb <vanbrght@gmail.com>
2025-09-08 21:49:02 +0000

    Rename UserDetails to UserProfile and update imports

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,7 +1,7 @@
-import UserDetails from './UserDetails';
+import UserProfile from './UserProfile';
 
 function UserInfo() {
-  return <UserDetails />;
+  return <UserProfile />;
 }
 
 export default UserInfo;

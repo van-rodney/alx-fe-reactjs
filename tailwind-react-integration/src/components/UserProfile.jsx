fb7565ceece9c361608460a30d751b4e903c0fef
verb <vanbrght@gmail.com>
2025-09-24 22:18:04 +0000

    feat: add responsive styling to UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-sm md:text-base text-gray-600">
